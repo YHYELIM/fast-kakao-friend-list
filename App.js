a0a68e421ea73e4afbc06cf5f188d81f5083c963
chaechaebing <chaechaebing@gmail.com>
2024-06-21 14:50:43 +0900

    react-native-safe-area-context로 수정

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import { Platform,  StyleSheet, Text, View } from 'react-native';
 import Header from './src/Header';
 import { getStatusBarHeight, getBottomSpace } from 'react-native-iphone-x-helper';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
+//상단바 높이 게산 : iphone 상단 노치 영역 포함
+//히단 안전 영역 높이 계산  
 const statusBarHeight = getStatusBarHeight(true);
 const bottomSpace = getBottomSpace();
 
 //위 아래 높이 확인 콘솔 
 console.log(`${Platform.OS}: ${statusBarHeight}, ${bottomSpace}`);
 
-
+//상단, 좌우 가장자리에서 안전 영역 적용 
+//SafeAreaProvider : 안전 영역 컨텍스트 제공 
 export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-  
-  <Header/>
-     
+    <SafeAreaProvider>
+    <SafeAreaView style={{flex: 1 }} edges={['top','right', 'left']} >
+      <Header/>
     </SafeAreaView>
+    
+    </SafeAreaProvider>  
   );
 }
 
@@ -28,3 +33,4 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-start',
   },
 });
+
