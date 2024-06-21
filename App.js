bbbedf455c9e89262d8fe2fe649eb5b43896e033
chaechaebing <chaechaebing@gmail.com>
2024-06-21 17:17:40 +0900

    친구리스트 Toggle 로직

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import Margin from './src/Margin';
 import Division from './src/Division';
 import FriendSection from './src/FriendSection';
 import FriendList from './src/FriendList';
+import { useState } from 'react';
 
 //상단바 높이 게산 : iphone 상단 노치 영역 포함
 //히단 안전 영역 높이 계산  
@@ -24,11 +25,18 @@ console.log(`${Platform.OS}: ${statusBarHeight}, ${bottomSpace}`);
 //상단, 좌우 가장자리에서 안전 영역 적용 
 //SafeAreaProvider : 안전 영역 컨텍스트 제공 
 export default function App() {
+  
+  const [isOpened, setIsOpened]= useState(true);
+  //기존 state가 열려있는것으로 세팅 
+
   const onPressArrow =()=>{
-    console.log('clicked arrow');
+    //console.log('clicked arrow');
+    setIsOpened(!isOpened);// 현재 열린 값에서 반대값으로 세팅 
   }
   
   
+
+
   return (
     <SafeAreaProvider>
     <SafeAreaView style={styles.container} edges={['top','right', 'left','bottom']} >
@@ -47,11 +55,12 @@ export default function App() {
       <FriendSection
         friendProfileLen={friendProfiles.length}
         onPressArrow={onPressArrow}
-      
+        isOpened={isOpened}  
       />
 
       <FriendList
         data={friendProfiles}
+        isOpened={isOpened}
       />
     </SafeAreaView>
 
@@ -65,7 +74,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     paddingTop: statusBarHeight,
-    paddingHorizontal:15,
+    paddingHorizontal:15, 
     backgroundColor: '#fff',
     justifyContent: 'flex-start',
   },
--- src/FriendList.js
+++ src/FriendList.js
@@ -9,29 +9,86 @@ const bottomSpace = getBottomSpace();
 //1. map 함수 사용
 //하나씩 순차적으로 순회 후 배열의 길이 만큼 리턴
  export default(props)=>{
-    return(
-        <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom:bottomSpace}}>
-            {/*첫번째 오브젝트
-            uri: "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__480.jpg",
-            name: "김민호",
-            introduction: "Minho Kim",
-            */}
-            {/*각 아이템의 키에는 인덱스를 넣어줌 */}
+   /*case 1. 삼항연산자 사용  */
+    // return props.isOpened ? (
+    //     <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom:bottomSpace}}>
+    //         {/*첫번째 오브젝트
+    //         uri: "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__480.jpg",
+    //         name: "김민호",
+    //         introduction: "Minho Kim",
+    //         */}
+    //         {/*각 아이템의 키에는 인덱스를 넣어줌 */}
           
-            {props.data.map((item,index) => (
-                  <View>
-                <Profile
-                key={index}
-                uri={item.uri}
-                name={item.name}
-                introduction={item.introduction}  
-                />
-                <Margin height={13}/>
-                 </View>
+    //         {props.data.map((item,index) => (
+    //             <View key={index}>
+    //             <Profile
+    //             uri={item.uri}
+    //             name={item.name}
+    //             introduction={item.introduction}  
+    //             />
+    //             <Margin height={13}/>
+    //              </View>
                  
-            ))}
-           
-            
-        </ScrollView>
-    )
-}
+    //         )
+    //         )}
+
+    //     </ScrollView>
+    // ) : null;
+
+    /*Case 2. if문으로 먼저 예외 처리 */
+//     if(!props.isOpened) return null;
+//     return (
+//         <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom:bottomSpace}}>
+//             {/*첫번째 오브젝트
+//             uri: "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__480.jpg",
+//             name: "김민호",
+//             introduction: "Minho Kim",
+//             */}
+//             {/*각 아이템의 키에는 인덱스를 넣어줌 */}
+          
+//             {props.data.map((item,index) => (
+//                 <View key={index}>
+//                 <Profile
+//                 uri={item.uri}
+//                 name={item.name}
+//                 introduction={item.introduction}  
+//                 />
+//                 <Margin height={13}/>
+//                  </View>
+                 
+//             )
+//             )}
+
+//         </ScrollView>
+//     );
+// };
+/* Case 3. && 이용  */
+//만약 
+//false && 1 = false
+//true && 2 =2 앞에 값이 true이므로 다음 값을 확인함
+//isOpened가 true일때 렌더링 해줌 
+return props.isOpened && (
+    <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{paddingBottom:bottomSpace}}>
+        {/*첫번째 오브젝트
+        uri: "https://cdn.pixabay.com/photo/2017/08/30/01/05/milky-way-2695569__480.jpg",
+        name: "김민호",
+        introduction: "Minho Kim",
+        */}
+        {/*각 아이템의 키에는 인덱스를 넣어줌 */}
+      
+        {props.data.map((item,index) => (
+            <View key={index}>
+            <Profile
+            uri={item.uri}
+            name={item.name}
+            introduction={item.introduction}  
+            />
+            <Margin height={13}/>
+             </View>
+             
+        )
+        )}
+
+    </ScrollView>
+);
+};
